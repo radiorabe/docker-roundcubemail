9f2854f85d81dd77b0142f409b254f32353cf9c7
Lucas Bickel <hairmare@purplehaze.ch>
2019-01-13 16:33:53 +0100

    :bug: unconditionally overwrite plugin config

--- rabe-docker-entrypoint.sh
+++ rabe-docker-entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
-  if [ ! -e /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php ]; then
-    echo "Write config to /var/roundcube/config/singleuserautologin.php"
-    echo "<?php
-    \$config['singleuserautologon_hostname'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_HOSTNAME}';
-    \$config['singleuserautologon_username'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_USERNAME}';
-    \$config['singleuserautologon_password'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_PASSWORD}';
-    " >  /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php
-  else
-    echo "WARNING: /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php already exists."
-    echo "ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_* environment variables have been ignored."
-  fi
+  echo "Write config to /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php"
+  echo "<?php
+  \$config['singleuserautologon_username'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_USERNAME}';
+  \$config['singleuserautologon_password'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_PASSWORD}';
+  " > /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php
 fi
 
 exec /docker-entrypoint.sh "$@"
