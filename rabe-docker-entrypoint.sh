e16b393d913bc27634d3c42d7cd7c2626ab8eb76
Lucas Bickel <hairmare@purplehaze.ch>
2019-01-13 16:39:56 +0100

    :bug: config naming

--- rabe-docker-entrypoint.sh
+++ rabe-docker-entrypoint.sh
@@ -3,8 +3,8 @@
 if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
   echo "Write config to /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php"
   echo "<?php
-  \$config['singleuserautologon_username'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_USERNAME}';
-  \$config['singleuserautologon_password'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_PASSWORD}';
+  \$config['singleuserautologin_username'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_USERNAME}';
+  \$config['singleuserautologin_password'] = '${ROUNDCUBEMAIL_SINGLEUSERAUTOLOGIN_PASSWORD}';
   " > /usr/src/roundcubemail/plugins/singleuserautologin/config.inc.php
 fi
 
